3907f2c848cea2a166b04b6445fc8f43e5350ff0
Giovanni Cappellotto <potomak84@gmail.com>
2015-03-05 00:17:38 -0500

    Add error and loading handling

--- gist-txt.js
+++ gist-txt.js
@@ -1,4 +1,8 @@
 window.onpopstate = function(event) {
+  if (typeof files === 'undefined') {
+    return init();
+  }
+
   handleNewScene(document.location.hash);
 };
 
@@ -31,7 +35,20 @@ var renderPage = function(content) {
 };
 
 var getFileContent = function(scene) {
-  return $.get(files[scene + '.markdown']['raw_url']);
+  return $.Deferred(function(defer) {
+    var file = files[scene + '.markdown']
+
+    if (typeof file === 'undefined') {
+      defer.reject('Scene not found');
+      return;
+    }
+
+    $.get(file.raw_url)
+      .done(defer.resolve)
+      .fail(function(jsXHR) {
+        defer.reject(jsXHR.statusText);
+      });
+  }).promise();
 };
 
 var render = function(markdown) {
@@ -44,11 +61,20 @@ var render = function(markdown) {
 };
 
 var loadAndRender = function(scene) {
+  toggleError(false);
+  toggleLoading(true);
+
   return getFileContent(scene)
     .then(render)
     .then(renderPage)
     .then(updateContentLinks)
-    .then(handleInternalLinks);
+    .then(handleInternalLinks)
+    .fail(function(errorMessage) {
+      toggleError(true, errorMessage);
+    })
+    .always(function() {
+      toggleLoading(false);
+    });
 };
 
 var parse = function(hash) {
@@ -64,15 +90,32 @@ var parse = function(hash) {
   return scene;
 };
 
+var toggleError = function(display, errorMessage) {
+  $('#error').html('Error: ' + errorMessage).toggle(display);
+};
+
+var toggleLoading = function(display) {
+  $('#loading').toggle(display);
+};
+
+var init = function() {
+  var scene = parse(document.location.hash);
+
+  $.getJSON('https://api.github.com/gists/' + gistId)
+    .done(function(gist) {
+      $('a#source').attr('href', 'https://gist.github.com/' + gistId).html(gistId);
+      $('footer').show();
+      files = gist.files;
+      loadAndRender(scene);
+    })
+    .fail(function(jsXHR) {
+      toggleLoading(false);
+      toggleError(true, jsXHR.statusText);
+    });
+};
+
 var gistId;
 var files;
 
-$(function() {
-  var scene = parse(document.location.hash);
-
-  $.getJSON('https://api.github.com/gists/' + gistId).done(function(gist) {
-    $('a#source').attr('href', 'https://gist.github.com/' + gistId).html(gistId);
-    files = gist.files;
-    loadAndRender(scene);
-  });
-});
+// Start the engine
+$(init);
